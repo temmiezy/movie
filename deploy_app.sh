0f264c08009df73a4ac3fa96d42b25749405ae31
kenny <kenny@appworkstudio.com>
2021-08-01 15:46:28 -0500

    latest

--- deploy_app.sh
+++ deploy_app.sh
@@ -3,7 +3,7 @@
 # Put this on a Server
 # run chmod +x deploy_app.sh to make the script executable
 # 
-# Execute this script:  ./deploy_app.sh ariv3ra/python-circleci-docker:$TAG
+# Execute this script:  ./deploy_app.sh ariv3ra/movie-docker:$TAG
 # Replace the $TAG with the actual Build Tag you want to deploy
 #
 ########################################
